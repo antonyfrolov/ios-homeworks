10625b4ff46a2c2e9100b7844b8e9fcd58700b0f
Anton Frolov <frolan@ya.ru>
2022-04-05 15:19:55 +0300

    добавлен PostViewController

--- Navigation/Navigation/FeedViewController.swift
+++ Navigation/Navigation/FeedViewController.swift
@@ -13,6 +13,20 @@ class FeedViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .systemMint
+        
+        let buttonPost = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        
+        buttonPost.setTitle("Пост", for: .normal)
+        buttonPost.backgroundColor = .black
+        buttonPost.center = view.center
+        buttonPost.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        
+        view.addSubview(buttonPost)
+    }
+    
+    @objc private func tapAction() {
+        let vc = PostViewController()
+        present(vc, animated: true)
     }
     
 }
---
+++ Navigation/Navigation/PostViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PostViewController.swift
+//  Navigation
+//
+//  Created by antonfrolov on 05.04.2022.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+        
+        navigationItem.title = "Пост"
+        
+    }
+    
+
+   
+
+}
