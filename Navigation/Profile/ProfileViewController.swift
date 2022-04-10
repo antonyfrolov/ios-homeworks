c02e07789c3717690a8b4519eba3c94085958051
Anton Frolov <frolan@ya.ru>
2022-04-10 15:54:30 +0300

    Some views was aded

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -11,9 +11,29 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    let phv = ProfileHeaderView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+       self.view.backgroundColor = .lightGray
+       // view.constraints = safe
+        
+       
+        
+        //phv.customize()
+      //  view.addSubview(phv.userPic)
+       // view.addSubview(phv.textLabel)
+        
+        
+        view.addSubview(phv)
+        
     }
+    
+    override func viewWillLayoutSubviews() {
+        phv.frame = view.safeAreaLayoutGuide.layoutFrame
+    }
+    
+    
     
 }
