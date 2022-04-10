713ff27693e9f62dac67ac565ef006d0beeee3a6
Anton Frolov <frolan@ya.ru>
2022-04-10 06:38:06 +0300

    ProfileHeaderView was added

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by antonfrolov on 10.04.2022.
+//
+
+import Foundation
+import UIKit
+
+
+class ProfileHeaderView: UIView {
+    
+}
